8f340510cefc2b11c015e2bd41bacfbfe90563fe
jake11212 <jakeeshelman1@gmail.com>
2024-07-11 00:53:22 -0500

    working read/write functions as well as init

--- src/main.c
+++ src/main.c
@@ -8,8 +8,5 @@ int main(void)
         uint8_t read_buffer[RTC_TIME_REGISTER_SIZE];
         uint8_t write_buffer[RTC_TIME_REGISTER_SIZE] = {0,0,0,0,0,0,0};
         initialize_RTC(time_array);
-        read_register(read_buffer, RTC_TIME_REGISTER_SIZE, RTC_TIME_REGISTER_ADDRESS);
-        write_register(write_buffer, 7, 4);
-        read_register(read_buffer, RTC_TIME_REGISTER_SIZE, RTC_TIME_REGISTER_ADDRESS);
         return 0;
 }
