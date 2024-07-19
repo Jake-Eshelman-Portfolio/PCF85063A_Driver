ae6a5844afefe85f7ced156ed2d48eafb2cf23a9
jake11212 <jakeeshelman1@gmail.com>
2024-07-19 16:39:21 -0500

    merged with test branch, both builds successful, all tests passing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
 #include "PCF85063A.h"
+
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 int main(void)
 {
@@ -10,6 +12,9 @@ int main(void)
         if (ret != RTC_SUCCESS) {
                 LOG_INF("RTC initialzation failed");
                 return RTC_ERROR_DEVICE_SETUP;
+        }
+        else {
+                LOG_INF("RTC initialized successfully");
         }
                  
         return RTC_SUCCESS;
